07ae91144109ed35b10c9de460059b554cadcbbf
Cynthia <CynthiaSpinner@gmail.com>
2024-04-18 14:41:22 -0500

    updated Line 25

--- SQL_Exercise_1.sql
+++ SQL_Exercise_1.sql
@@ -9,7 +9,7 @@ SELECT * FROM products WHERE NOT Price = '11.99';
 -- find all products and sort them by price from greatest to least
 SELECT * FROM products ORDER BY price DESC;
 -- find all employees who don't have a middle initial
-SELECT * FROM employees WHERE MiddleInitial = NULL;
+SELECT * FROM employees WHERE MiddleInitial IS NULL;
 -- find distinct product prices
 SELECT DISTINCT price FROM products; 
 -- find all employees whose first name starts with the letter ‘j’
@@ -22,6 +22,6 @@ SELECT * FROM Products WHERE OnSale;
 -- find the average price of all products
 SELECT AVG(price) AS "Average of all Prices" FROM products;
 -- find all Geek Squad employees who don't have a middle initial
-SELECT * FROM employees WHERE title = "Geek Squad";
+SELECT * FROM employees WHERE Title = "Geek Squad" AND Middleinitial IS NULL;
 -- find all products from the products table whose stock level is in the range -- of 500 to 1200. Order by Price from least to greatest. Hint: Use the between keyword
 SELECT * FROM products WHERE Stocklevel BETWEEN 500 AND 1200 ORDER BY price;
